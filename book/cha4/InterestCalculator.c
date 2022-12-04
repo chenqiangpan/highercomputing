8099b2952e586f08a53c23b23cbcd73adc7e676d
qiangpan chen <chenqiangpan@gmail.com>
2022-12-05 10:21:40 +1100

    lecture 13

---
+++ book/cha4/InterestCalculator.c
@@ -0,0 +1,8 @@
+#include <stdion.h>
+
+
+
+int main ()
+{
+
+}
--- lectures/lecture13.c
+++ lectures/lecture13.c
@@ -7,19 +7,19 @@ int main (int argc, const char * argv[])
 {
 	byte *pointer; //giving type by example
 
-	byte b1 =0;
+	byte b1 =1;
+	byte b2 =2;
 	int i1 = 0;
 	int i2 = 0;
-	byte b2 =0;
-	byte b3 =0;
-	byte b4 =0;
-	byte b5 =0;
-	byte b6 =0;
+	byte b3 =3;
+	byte b4 =4;
+	byte b5 =5;
+	byte b6 =6;
 
 	pointer = &b6;
 	printf("pointer has value of %p \n ", pointer);
-	//printf("biggest float on this machine %ld \n",LONG_MAX);
-		
+	printf("the byte it points to contains %d\n",*pointer);
+
 	printf("b1 (%p) = %x\n", &b1, b1);
 	printf("b2 (%p) = %x\n", &b2, b2);
 	printf("i1 (%p) = %d\n", &i1, i1);
