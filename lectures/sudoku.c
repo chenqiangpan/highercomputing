55c01a70fe27d7febf6363b4de2ccfa54a048314
qiangpan chen <chenqiangpan@gmail.com>
2022-12-06 16:08:01 +1100

    update sudoku

--- lectures/sudoku.c
+++ lectures/sudoku.c
@@ -12,136 +12,234 @@
 
 typedef char value;
 typedef int cell;
-typedef value sudokuGrid[GRID_SIZE+1];
+typedef value sudokuGrid[GRID_SIZE + 1];
 
-int hasSolution(sudokuGrid game);
-void readGame(sudokuGrid sample);
-void showGame(sudokuGrid sample);
-int isFull(sudokuGrid game);
-cell getaEmptyCell(sudokuGrid game);
-int isLegal(sudokuGrid game, cell candidateCell, value trialValue);
-void setCell(sudokuGrid game, cell candidateCell, value trialValue);
-void clearCell(sudokuGrid game, cell candidateCell);
+int hasSolution (sudokuGrid game);
+void readGame (sudokuGrid *sample);
+void showGame (sudokuGrid sample);
+int isFull (sudokuGrid game);
+cell getaEmptyCell (sudokuGrid game);
+int isLegal (sudokuGrid game, cell candidateCell, value trialValue);
+void setCell (sudokuGrid game, cell candidateCell, value trialValue);
+void clearCell (sudokuGrid game, cell candidateCell);
 
 
-int main(int argc, const char * argv[])
-
+int
+main (int argc, const char *argv[])
 {
-	
-	sudokuGrid sample;
-	readGame(sample);
 
-	if(hasSolution(sample))
-	{
-		showGame(sample);
-	}
+  sudokuGrid sample;
+  readGame (&sample);
+  //showGame(sample);
 
-	return 0;
+  if (hasSolution (sample))
+  {
+      printf("\n the game has solution!");
+      
+
+
+
+
+
+  }
+  else
+  {
+    printf("\n the game has NO solution!");
+  }
+
+     printf ("\n let's see what is inside the array Again: \n");
+
+  for (int i = 0; i < GRID_SIZE ; i++)
+    {
+      printf ("%c", sample[i]);
+    }
+
+  return 0;
 
 }
 
 
-int hasSolution(sudokuGrid game)
+int
+hasSolution (sudokuGrid game)
 {
-	int solved ;
-	cell candidateCell;
-	value trialValue;
+  int solved;
+  cell candidateCell;
+  value trialValue;
 
 
-	if(isFull(game))
-	{
-		solved = TRUE;
-	}else
-	{
-		candidateCell = getaEmptyCell(game);
-		trialValue = MIN_VALUE;
-		solved = FALSE;
+  if (isFull (game))
+    {
+      printf("\n the game is FULL! \n");
+      solved = TRUE;
+    }
 
-		while (!solved &&(trialValue <= MAX_VALUE))
-		{
+  else
+    {
+        printf("\n the game is NOT FULL! \n");
+      candidateCell = getaEmptyCell (game);
 
-			if(isLegal(game, candidateCell, trialValue))
-			{
+      trialValue = MIN_VALUE;
 
+      solved = FALSE;
 
-				setCell(game, candidateCell, trialValue);
+      while (!solved && (trialValue <= MAX_VALUE))
+	      {
 
-					if(hasSolution(game))
-					{
-						solved = TRUE;
-					}
-					else
-					{
-						clearCell(game, candidateCell);
-					}
+      	  if (isLegal (game, candidateCell, trialValue))
+	        {
 
 
-			}
-			trialValue ++;
-		}
+	          setCell (game, candidateCell, trialValue);
+
+	          if (hasSolution (game))
+		        {
+		          solved = TRUE;
+		        }
+	          else
+		        {
+		          clearCell (game, candidateCell);
+		        }
 
 
-	}
+    	    }
+	        trialValue++;
+	      }
 
-	return solved;
+
+    }
+
+  return solved;
 
 }
 
 
-void readGame(sudokuGrid sample)
+
+
+// check----------read a game from txt and write it to sample array
+void
+readGame (sudokuGrid *sample)
 {
 
-	FILE *textfile;
-	char ch;
-	int index = 0;
+  FILE *textfile;
+  char ch;
+  int index = 0;
 
-	textfile = fopen("sample.txt", "r");
+  textfile = fopen ("sample.txt", "r");
 
-	while((ch = fgetc(textfile))!=EOF)
-	{
-		putchar(ch);
+  printf ("\n this is the original sample:  \n");
 
-		sample[index] = ch;
-		index ++;
-	}
+  while ((ch = fgetc (textfile)) != EOF)
+    {
+      putchar (ch);
 
-
-	printf("\n let's see what is inside the array: \n");
-
-	for(int i = 0; i < GRID_SIZE+1; i++)
-	{
-		printf("%c", sample[i]);
-	}
+      *sample[index] = ch;
+      index++;
+    }
 
 
-	fclose(textfile);
+  printf ("\n let's see what is inside the array: \n");
+
+  for (int i = 0; i < GRID_SIZE ; i++)
+    {
+      printf ("%c", *sample[i]);
+    }
+  fclose (textfile);
 }
 
-void showGame(sudokuGrid sample)
-{
-
-}
-int isFull(sudokuGrid game)
-{
-
-	return 0;
-
-}
-cell getaEmptyCell(sudokuGrid game)
-{
-	cell emptyCell = 1;
-
-	return emptyCell;
-}
-int isLegal(sudokuGrid game, cell candidateCell, value trialValue)
-{
-	return 0;
-}
-void setCell(sudokuGrid game, cell candidateCell, value trialValue)
+void
+showGame (sudokuGrid sample)
 {
 
 }
-void clearCell(sudokuGrid game, cell candidateCell)
+
+
+
+// check---------- check sample game is full or not
+int
+isFull (sudokuGrid game)
 {
 
+  for (int i = 0; i <GRID_SIZE +1; i++)
+  {
+    if (game[i]=='.')
+    {
+     
+      return FALSE;
+    }
+    
+  }
+  
+
+  return TRUE;
+
+}
+
+
+// check---------- get the first empty cell (index) of the game
+cell
+getaEmptyCell (sudokuGrid game)
+{
+  cell emptyCell = 0;
+
+  while (game[emptyCell]!='.')
+  {
+    emptyCell++;
+  }
+
+  printf("\n the empty cell is %d \n ", emptyCell);
+  
+  return emptyCell;
+}
+
+
+//not verify yet ------ see if a candidate value is valid
+int
+isLegal (sudokuGrid game, cell candidateCell, value trialValue)
+{
+  int row;
+  int column; 
+  row = candidateCell / 9;
+  column = candidateCell % 9;
+
+  //scan vertical
+  for (int i = 0; i <9 ; i++)
+  {
+
+    if(game[(9 * i) + row] == game[candidateCell]  )
+    {
+      if((9 * i) + row != candidateCell)
+      
+          return FALSE;
+    }
+  }
+  //scan horizontal
+  for (int j = 0; j <9 ; j++)
+  {
+
+    if(game[(column*9)+j] == game[candidateCell] )
+    {
+      if((column*9)+j != candidateCell)
+      
+          return FALSE;
+    }
+  }
+
+  //square group
+  //
+  //
+  return TRUE;
+}
+
+
+// put trial value into the candidate cell
+void
+setCell (sudokuGrid game, cell candidateCell, value trialValue)
+{
+    game[candidateCell] = trialValue;
+}
+
+void
+clearCell (sudokuGrid game, cell candidateCell)
+{
+  game[candidateCell] = '.'; 
 }
